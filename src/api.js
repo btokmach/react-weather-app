1226e55b7fde7fa9e5af37f76c146dcf0e7a6bf6
btokmach <btokmach@gmail.com>
2022-07-20 18:52:37 +0300

    fetching and mapping data from API

--- src/api.js
+++ src/api.js
@@ -8,3 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6f415890dc7c441203b8c22a7ae6a9ce";
