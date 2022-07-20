6bb03cc3f1997688bd2c2bfa68ba39cfc15bf064
btokmach <btokmach@gmail.com>
2022-07-20 15:48:16 +0300

    builded city search component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Search from './components/search/Search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
   return (
     <div className="container">
-      <Search/>
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3ba106329cmsh57a3c8d45af185cp184a13jsndd15c6832955',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
